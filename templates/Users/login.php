93ffa8d26a02adec84ff38c8109a36865e8351cc
Guilherme Reinaldo <guilhermepreinaldo@gmail.com>
2025-07-08 20:18:21 -0300

    testando tudo

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -1,5 +1,5 @@
 <div class="users form">
-    <h2>Login</h2>
+    <h2>Loginaokd</h2>
     <?= $this->Form->create() ?>
     <fieldset>
         <?= $this->Form->control('email', ['required' => true, 'label' => 'Email']) ?>
